fcfebd3b052eb2511c8c63d706eebd1775890ee4
Samson <zeniths.era@gmail.com>
2019-09-21 15:08:51 -0400

    Testing push to remote branch

--- market/urls.py
+++ market/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('user/<str:username>/products', views.Products, name='user-shop'),
     path('product/<uuid:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('product/create/', views.ProductCreate.as_view(), name='product-create'),
+    # 
 ]
